fb8618ddb0d31ff8fb595e1c840662e789f72c57
Luxter77 <Luxter77@users.noreply.github.com>
2022-10-16 03:31:27 -0300

    package renamed to hijacked-chain

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-   name             =   'hijacked_lib',
+   name             =   'hijacked_chain',
    version          =   '0.1',
    description      =   "Hijacked Node's NG text generation and parsing module",
    license          =   "0BSD",
@@ -12,7 +12,7 @@ setup(
    author           =   'Luxter77',
    author_email     =   'Luxter77@eggg.tk',
    url              =   "https://eggg.tk/",
-   packages         =   ['hijacked_lib'],
+   packages         =   ['hijacked_chain'],
    install_requires =   ['hijacked_log~=0.1', 'numpy~=1.23.4', 'tqdm~=4.64.0'],
    scripts          =   ['scripts/hl-trans-back.py'],
 )
